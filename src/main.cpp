5bc4a1816fa71e606de97085619fae5a81c8b111
kierankai11 <kierantankc@gmail.com>
2024-10-31 20:12:52 +0800

    added auton and formatted code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include "main.h"
+#include "pros/rtos.h"
 
 bool clamp_up = false;
 bool forceClampRelease = false;
+
+void moveRPM(int left, int right) {
+  Motor frontRight(DRIVE_FR_P);
+  Motor frontLeft(DRIVE_FL_P);
+  Motor backRight(DRIVE_BR_P);
+  Motor backLeft(DRIVE_BL_P);
+
+  frontRight.move_velocity(right);
+  frontLeft.move_velocity(left);
+  backRight.move_velocity(right);
+  backLeft.move_velocity(left);
+}
 
 void clamp_tilt_task_fn(void *params) {
   Controller master(E_CONTROLLER_MASTER);
@@ -10,20 +23,20 @@ void clamp_tilt_task_fn(void *params) {
 
   while (true) {
     if (clamp_up) {
-        error = CLAMP_TILT_DEGREES - clamp.get_position();
-		    master.print(0, 0, "%f", error);
+      error = CLAMP_TILT_DEGREES - clamp.get_position();
+      master.print(0, 0, "%f", error);
+    } else {
+      if (forceClampRelease) {
+        error = CLAMP_FORCE_DEGREES - clamp.get_position();
+        forceClampRelease = false;
+        clamp_up = false;
       } else {
-        if (forceClampRelease) {
-          error = CLAMP_FORCE_DEGREES - clamp.get_position();
-          forceClampRelease = false;
-          clamp_up = false;
-        } else {
-          error = CLAMP_DEFAULT_DEGREES - clamp.get_position();
-        }
+        error = CLAMP_DEFAULT_DEGREES - clamp.get_position();
       }
-      clamp.move((error * CLAMP_TILT_KP) + 10);
-      delay(20);
     }
+    clamp.move((error * CLAMP_TILT_KP) + 10);
+    delay(20);
+  }
 }
 
 void initialize() {
@@ -57,32 +70,43 @@ void disabled() {}
 void competition_initialize() {}
 
 void autonomous() {
-  Motor frontRight(DRIVE_FR_P);
-  Motor frontLeft(DRIVE_FL_P);
-  Motor backRight(DRIVE_BR_P);
-  Motor backLeft(DRIVE_BL_P);
-
   Motor intakeF(INTAKE_F);
   Motor intakeC(INTAKE_C);
-  Motor clamp(CLAMP_P);
 
-  frontRight.move_velocity(-200);
-  frontLeft.move_velocity(-200);
-  backRight.move_velocity(-200);
-  backLeft.move_velocity(-200);
-
+  // Move backwards towards clamp
+  moveRPM(-200, -200);
   delay(900);
+  moveRPM(0, 0);
 
-  frontRight.move_velocity(0);
-  frontLeft.move_velocity(0);
-  backRight.move_velocity(0);
-  backLeft.move_velocity(0);
-  
-  //
+  // Activate clamp to get stake
   clamp_up = true;
-  
+  delay(1000);
+
+  // Turn 90 degrees to face a donut (lots of tuning required)
+  moveRPM(150, -150); // for red negative corner, blue positive corner
+  delay(300);
+  moveRPM(0, 0);
+
+  // Move forward and start intake
+  moveRPM(200, 200);
+  intakeF.move_velocity(600);
+  delay(700);
+
+  // Score two donuts
+  moveRPM(0, 0);
+  intakeC.move_velocity(300);
+  for (int i = 0; i < 6; i++) { // Checks if conveyer is stuck 6 times at intervals of 1s. Total of 6 seconds of conveyer moving.
+    if (intakeC.get_actual_velocity() <= 50) {
+      intakeC.move_velocity(0);
+      break;
+    } else {
+      delay(1000);
+    }
+  }
+
+  // Stop
+  intakeC.move_velocity(0);
   intakeF.move_velocity(0);
-  // for next time
 }
 
 void opcontrol() {
@@ -109,8 +133,10 @@ void opcontrol() {
     backLeft.move(left);
     frontLeft.move(left);
 
-    intakeC.move(127*(master.get_digital(E_CONTROLLER_DIGITAL_R1)-master.get_digital(E_CONTROLLER_DIGITAL_R2)));
-    intakeF.move(127*(master.get_digital(E_CONTROLLER_DIGITAL_R1)-master.get_digital(E_CONTROLLER_DIGITAL_R2)));
+    intakeC.move(127 * (master.get_digital(E_CONTROLLER_DIGITAL_R1) -
+                        master.get_digital(E_CONTROLLER_DIGITAL_R2)));
+    intakeF.move(127 * (master.get_digital(E_CONTROLLER_DIGITAL_R1) -
+                        master.get_digital(E_CONTROLLER_DIGITAL_R2)));
 
     if (master.get_digital_new_press(DIGITAL_L1)) {
       clamp_up = !clamp_up;
@@ -118,7 +144,7 @@ void opcontrol() {
 
     if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A)) {
       clamp_up = false;
-      forceClampRelease  = true;
+      forceClampRelease = true;
     }
 
     delay(20); // Run for 20 ms then update
